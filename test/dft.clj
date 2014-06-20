00d7a11bc4db91ef6de43eacb9aa52a3cec34d17
Juan Facorro <Juan Facorro>
2014-06-20 00:36:14 -0300

    Added some test for checking the rebuilding of the original function based on the DFT's result.

--- test/dft.clj
+++ test/dft.clj
@@ -2,19 +2,28 @@
   (:require [dft :refer [dft build-original-fn to-polar]])
   (:use [incanter core stats charts]))
 
-(let [begin  0
-      end    (* 4 Math/PI)
-      f      #(+ 0 (Math/sin %))
-      g      (dft f begin end 20)
-      f'     (build-original-fn g)
-      pairs  (->> g
-               (map to-polar)
-               (mapcat (juxt :arg :mod))
-               vec)]
+(let [begin   (* 4 Math/PI -1)
+      end     (* 4 Math/PI)
+      samples 32
+      f       #(+ 0 (Math/sin %))
+;;      f      #(+ 0 (* 0.5 %))
+      g       (dft f begin end samples)
+      f'      (build-original-fn g)
+      pairs   (->> g
+                (map to-polar)
+                (mapcat (juxt :arg :mod))
+                vec)]
   (doto 
     (scatter-plot
-      (take-nth 2 pairs)
-      (take-nth 2 (drop 1 pairs)))
+      (range samples)
+      (map :img g)
+      :title "Imaginary")
+    view)
+  (doto 
+    (scatter-plot
+      (range samples)
+      (map :real g)
+      :title "Real")
     view)
   (doto
     (function-plot f begin end)
